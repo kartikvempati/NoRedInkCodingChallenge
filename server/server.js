5327b869e36a16f85d2246459bc53d38a3b01251
Kartik <kvempati11@gmail.com>
2016-04-01 12:42:04 -0700

    Set up node/express server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var cors = require('cors')
+var app = express();
+var port = process.env.PORT || 8000;
+
+app.use(express.static(__dirname + '/../client'));
+app.use(cors());
+
+
+var server = app.listen(port);
+
+
+module.exports = app;
